b12beb5d27f4791c28f4f22e462a8fb0488e7161
ShyMike <122023566+ImShyMike@users.noreply.github.com>
2025-12-02 13:53:40 +0000

    forgot to commit this

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -1,19 +1,28 @@
 dial = 50
 with open("2025/01/input.txt") as f:
     val = f.read().strip()
-password = 0
+password1 = 0
+password2 = 0
 items = val.split("\n")
 
 for item in items:
     dir = item[0]
     num = int(item[1:])
     
-    if dir == "R":
-        dial += num
-    elif dir == "L":
-        dial -= num
+    # i hate this but im way too sleepy to think of
+    # how to do this with math so a for loop it is
+    for _ in range(num):
+        if dir == "R":
+            dial += 1
+        elif dir == "L":
+            dial -= 1
+        
+        dial %= 100
+        
+        if dial == 0:
+            password2 += 1
+    
+    if dial == 0:
+        password1 += 1
 
-    if dial % 100 == 0:
-        password += 1
-
-print("password:", password)
+print(f"challenge 1: {password1} - challenge 2: {password2}")
